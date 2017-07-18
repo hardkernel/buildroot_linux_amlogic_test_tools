772f7536608f0c18dfa36834e7aab3ac2d9f707d
Haibing.An <haibing.an@amlogic.com>
2017-07-18 10:58:30 +0800

    test_tool: fix player test bug
    Change-Id: I96d2eb4b555e4f9da555f69c038c2524f2be6ba3
    Signed-off-by: Haibing.An <haibing.an@amlogic.com>

--- test_plan/player/test.sh
+++ test_plan/player/test.sh
@@ -1,39 +1,37 @@
 #!/bin/sh
 
-#Run cpu a53
+#Run as daemon
 /test_plan/player/cpuburn-a53 &
 
-#Change cpu freq
-/test_plan/player/cpu_freq_v1_0.sh & 
+/test_plan/player/cpu_freq_v1_0.sh &
 
 mount /dev/sda1 /mnt
 
-info()
-{
-	if [ $? -neq 0 ]; then
-		echo "play $3 wrong"
-	else
-		echo "play $3 OK"
-	fi
+info(){
+    if [ $? -neq 0 ]; then
+        echo "play $3 wrong"
+    else
+        echo "play $3 OK"
+    fi
 }
 
-while :
+while true
 do
-	aplay -D plug:2to8 /mnt/Hello.wav
-	info
+    aplay -D plug:2to8 /mnt/Hello.wav
+    info
 
-	aplay -D hw:0,2 /mnt/8ch_full.wav
-	info	
+    aplay -D hw:0,2 /mnt/8ch_full.wav
+    info
 
-	alsaplayer -d plug:2to8 /mnt/Hello.wav
-	info
+    alsaplayer -d plug:2to8 /mnt/Hello.wav
+    info
 
-	alsaplayer -d plug:2to8 /mnt/Hello.mp3
-	info
+    alsaplayer -d plug:2to8 /mnt/Hello.flac
+    info
 
-	alsaplayer -d plug:2to8 /mnt/Hello.flac
-	info
+    alsaplayer -d plug:2to8 /mnt/Hello.ogg
+    info
 
-	alsaplayer -d plug:2to8 /mnt/Hello.ogg
-	info
+    alsaplayer -d plug:2to8 /mnt/Hello.mp3
+    info
 done
