d4b010c021867ad10ae783efd5a2cda513e1b158
Yueguie He <yuegui.he@amlogic.com>
2017-08-16 14:46:30 +0800

    PD#149154: insmod ddr_window.ko error
    Change-Id: I197a30c1b97856b9f69023dac936546378dcb9df
    Signed-off-by: Yueguie He <yuegui.he@amlogic.com>

--- test_plan/ddr/ddr_window/ddr_window_test.sh
+++ test_plan/ddr/ddr_window/ddr_window_test.sh
@@ -6,7 +6,13 @@ echo "ddr window test start..."
 ###############################
 DDR_WINDOW_DIR=/test_plan/ddr/ddr_window
 
-insmod ${DDR_WINDOW_DIR}/ddr_window.ko
+kernel_version=$(uname -a | awk '{print $3}')
+ddr_window_path=/lib/modules/${kernel_version}/kernel/drivers/amlogic/ddr_window/ddr_window.ko
+
+echo "kernel: ${kernel_version}"
+echo "ddr window path: ${ddr_window_path}"
+
+insmod ${ddr_window_path}
 if [ $? -ne 0 ]
 then
 	echo "insmod ddr_window.ko failure"
