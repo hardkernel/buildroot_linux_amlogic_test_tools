185cb1bb10e5cefc1669a55f30dcf93ec635cd24
Yueguie He <yuegui.he@amlogic.com>
2017-11-14 15:05:36 +0800

    audio: update audio test function
    1 update pdm in & tdm out
    2 update pdm in & line out
    3 update line in & line out
    
    Change-Id: I4720a99b78cd9a0ec550df4ce19dd8dc5bd727d1
    Signed-off-by: Yueguie He <yuegui.he@amlogic.com>

--- test_plan/audio/audio_test.sh
+++ test_plan/audio/audio_test.sh
@@ -9,7 +9,7 @@ taskkill()
 	if [ $# -ne 2 ]; then
 	  PID=`ps ax | grep $1 | awk '{if ($0 !~/grep/) {print $1}}'`
 		#echo "PID=$PID"
-		if [ -n "$PID" ]; then 
+		if [ -n "$PID" ]; then
 			kill -9 $PID >/dev/null 2>&1
                         return 0
 		else
@@ -32,9 +32,23 @@ delay()
 		done
 		echo "stop"
                 echo ""
-		return 0 			
-	fi	
+		return 0
+	fi
 	return 1
+}
+
+
+mount_u_disk()
+{
+	#check udisk insert or not
+	ls /dev/sda1
+	while [ $? -ne 0 ]
+	do
+        echo "please insert your u disk."
+		sleep 2
+		ls /dev/sda1
+	done
+	echo "u disk inserted ."
 }
 
 pdm_in()
@@ -163,7 +177,7 @@ tdm_in_tdm_out()
     echo "*********************stop TDM_IN_TDM_OUT test*************************"
 }
 
-pdm_in_tdm_out()
+pdm_in_tdm_out_line_out()
 {
     echo ""
 	echo ""
@@ -171,8 +185,12 @@ pdm_in_tdm_out()
 	echo "******************start PDM_IN_TDM_OUT test**************************"
 	#tdm_out pdm_in_dir pdm_in
 
+	#mkdir pdm_in_tdm_out_line_out
+	mkdir /mnt/pdm_in_tdm_out_line_out
+
+
 	#set 8000 16000 44100 48000
-	local channel=8
+	local channel=2
 	ratelist="8000 16000 44100 48000"
 	bitlist="S16_LE S24_LE S32_LE"
 
@@ -183,25 +201,35 @@ pdm_in_tdm_out()
 			for j in $bitlist
 			do
 				echo "channel="$channel",rate="$i",bit=$j"
-				aplay -C -Dhw:0,3 -r $i -f $j -t wav -c $channel | aplay  -Dhw:0,2 &
+				arecord  -C -Dhw:0,3 -r $i -f $j  -c $channel -d 2 /mnt/pdm_in_tdm_out_line_out/test${i}_${j}.wav
+				aplay -Dhw:0,2 /mnt/pdm_in_tdm_out_line_out/test${i}_${j}.wav 
 				delay $DELAYS
 				kill_task
 			done
 		done
-		let channel=channel/2
+		if [ "${channel}"x == "2"x ]
+		then
+			echo "last channel."
+			break
+		else
+		    let channel=channel/2
+		fi
 	done
 	echo "*********************stop PDM_IN_TDM_OUT test*************************"
 }
 
 line_in_line_out()
 {
-	local channel=8
+	local channel=2
 	if [ $# -ne 2 ]; then
 	#mount -t vfat  /dev/sda1 /mnt
 	echo ""
 	echo ""
 	echo ""
 	echo "******************start LINE_IN and LINE_OUT  test**************************"
+
+    mkdir /mnt/line_in_line_out
+
 	#set 8000 16000 44100 48000 96000
 	#set S16_LE S24_LE S32_LE
 	ratelist="8000 16000 44100 48000"
@@ -214,12 +242,20 @@ line_in_line_out()
 			for j in $bitlist
 			do
 				echo "channel="$channel",rate="$i",bit=$j"
-				aplay -C -Dhw:0,2 -r $i -f $j -c 2 | aplay  -Dhw:0,2 &
+				arecord  -C -Dhw:0,2 -r $i -f $j  -c $channel -d 2 /mnt/line_in_line_out/test${i}_${j}.wav
+				aplay -Dhw:0,2 /mnt/line_in_line_out/test${i}_${j}.wav
+				#aplay -C -Dhw:0,2 -r $i -f $j -c 2 | aplay  -Dhw:0,2 &
 				delay 10
 				kill_task
 			done
 		done
-		let channel=channel/2
+		if [ "${channel}"x == "2"x ]
+		then
+			echo "last channel."
+			break
+		else
+		    let channel=channel/2
+	    fi
 	done
 	#umount /mnt
 	echo "*********************stop LINE_IN and LINE_OUT test*************************"
@@ -361,16 +397,16 @@ echo ""
 echo "****************************************************"
 echo "*     Amlogic A113 Platform Audio Case Test        *"
 echo "****************************************************"
-echo "*   tdm in test:                    [0]            *"
-echo "*   pdm in test:                    [1]            *"
-echo "*   tdm out test:                   [2]            *"
-echo "*   tdm in and tdm out test:        [3]            *"
-echo "*   pdm in and tdm out test:        [4]            *"
-echo "*   line in and line out test:      [5]            *"
-echo "*   spdif in and spdif out test:    [6]            *"
-echo "*   tdm_A_dummy test:               [7]            *"
-echo "*   tdm_B_dummy test:               [8]            *"
-echo "*   tdm_C_dummy test:               [9]            *"
+echo "*   pdm in and tdm_line out test:   [0]            *"
+#echo "*   pdm in test                     [1]            *"
+#echo "*   tdm out test:                   [2]            *"
+#echo "*   tdm in and tdm out test:        [3]            *"
+#echo "*   pdm in and tdm out test:        [4]            *"
+echo "*   line in and line out test:      [1]            *"
+#echo "*   spdif in and spdif out test:    [6]            *"
+#echo "*   tdm_A_dummy test:               [2]            *"
+#echo "*   tdm_B_dummy test:               [3]            *"
+#echo "*   tdm_C_dummy test:               [4]            *"
 echo "*   exit shell test:                [q]            *"
 echo "****************************************************"
 
@@ -378,46 +414,56 @@ echo ""
 echo -n  "choice your case:"
 read TEST_CASE
 
+# u disk init
+mount_u_disk
+
+#
 
 case $TEST_CASE in
     "0")
-        tdm_in
+		pdm_in_tdm_out_line_out
+        #tdm_in
     ;;
     "1")
-		pdm_in
+		line_in_line_out
+		#pdm_in
     ;;
     "2")
-	echo ""
-	echo ""
-	echo ""
-        echo "******************start TDM_OUT test**************************" 
-        tdm_out pcm_dir tdm_out
-        echo "******************stop TDM_OUT test**************************" 
+        tdm_a_dummy
+	#echo ""
+	#echo ""
+	#echo ""
+    #    echo "******************start TDM_OUT test**************************"
+    #    tdm_out pcm_dir tdm_out
+    #    echo "******************stop TDM_OUT test**************************"
     ;;
     "3")
-        tdm_in_tdm_out tdm_in_dir
+		tdm_b_dummy
+        #itdm_in_tdm_out tdm_in_dir
     ;;
     "4")
-        pdm_in_tdm_out pdm_in_dir
-    ;;
-    "5")
-        line_in_line_out
-    ;;
-    "6")
-        spdif_in_spdif_out
-    ;;
-    "7")
-	tdm_a_dummy
-    ;;
-    "8")
-	tdm_b_dummy
-    ;;
-    "9")
-	tdm_c_dummy
+		tdm_c_dummy
+       # pdm_in_tdm_out pdm_in_dir
     ;;
     "q")
-	echo "exit!"
+ 		echo "exit!"
+ 		#line_in_line_out
     ;;
+##   "6")
+#        spdif_in_spdif_out
+#    ;;
+#    "7")
+#	tdm_a_dummy
+#    ;;
+#    "8")
+#	tdm_b_dummy
+#    ;;
+#    "9")
+#	tdm_c_dummy
+#    ;;
+#    "q")
+#	echo "exit!"
+#    ;;
     *) echo "can't recognition this case"
     ;;
 esac
