3cc3951bc790d12be729838c64c062f48af10aae
Yueguie He <yuegui.he@amlogic.com>
2017-11-17 15:12:53 +0800

    update ddr test module
    1 adapte lede
    
    Change-Id: Ie433cbcb5effc91c513a04daf123baa8de0aafc8
    Signed-off-by: Yueguie He <yuegui.he@amlogic.com>

--- test_plan/ddr/ddr_window/ddr_window_test.sh
+++ test_plan/ddr/ddr_window/ddr_window_test.sh
@@ -18,12 +18,12 @@ function resource_copy()
 	else
 		mount /dev/sda /mnt
 	fi
-	
+
 	cd /mnt
 	echo "Please input the No. of platform"
 	read platform
 	mkdir ddr_windows_results_$platform
-	
+
 	echo "CP start!!"
 	cp /sdcard/ddr_window_* /mnt/ddr_windows_results_$platform/
 	echo "CP sync!!"
@@ -37,9 +37,53 @@ echo "A113 DDR Windows Test,Please input the numbers:
 
 read cast;
 
+ddr_window_test_lede()
+{
+        ###############################
+        echo "ddr window test start..."
+        ###############################
+        DDR_WINDOW_DIR=/test_plan/ddr/ddr_window
+        kernel_version=$(uname -a | awk '{print $3}')
+        ddr_window_path=/lib/modules/${kernel_version}/ddr_window.ko
+        echo "kernel: ${kernel_version}"
+        echo "ddr window path: ${ddr_window_path}"
+        insmod ${ddr_window_path}
+        if [ $? -ne 0 ]
+        then
+                echo "insmod ddr_window.ko failure"
+                exit 1
+        fi
+        ${DDR_WINDOW_DIR}/memcpy_test -f &
+        if [ $? -ne 0 ]
+        then
+                echo "FAILURE: memcpy_test aplication start error."
+                exit 1
+        fi
+        ${DDR_WINDOW_DIR}/ddr_window -f &
+        if [ $? -ne 0 ]
+        then
+                echo "FAILURE: ddr window aplication start error."
+                exit 1
+        fi
+
+}
+
+platform_handle()
+{
+	build_env=$(uname -a | awk '{print $2}')
+	case ${build_env} in
+	  "LEDE")
+		ddr_window_test_lede
+		;;
+	  "buildroot")
+		resource_ready
+		;;
+	esac
+}
+
 case ${cast} in
 	1)
-	resource_ready
+	platform_handle
 	;;
 	2)
 	resource_copy
